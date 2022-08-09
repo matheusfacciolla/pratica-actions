1d876c2af7f9d89954b465e37c0bcbc6047ed9a9
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-08-09 19:13:59 -0300

    feat: new console5

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
-  console.log("PASSEI AQUI! QUE ALEGRIA! teste1")
+  console.log("PASSEI AQUI! QUE ALEGRIA! teste2")
   const students = await prisma.student.findMany();
   res.send(students);
 });
